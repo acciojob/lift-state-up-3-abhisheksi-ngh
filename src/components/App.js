d91bbbda519d737ace1ea3121ca0ea96fcef23fe
abhishekashj <abhishekkumrsingh9585@gmail.com>
2025-05-16 10:32:22 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
 import React from "react";
 import './../styles/App.css';
+import ChildComponent1 from "./ChildComponent1";
+import ChildComponent2 from "./ChildComponent2";
 
 const App = () => {
+  const [selectOption , setSelectOption] = React.useState("");
+  const handleOptionChange=(option)=>{
+    setSelectOption(option);
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+      <h1>Parent Component</h1>
+      
+      <ChildComponent1 onSelect={handleOptionChange} />
+      <ChildComponent2 onSelect={handleOptionChange} />
+      <p>Selected Option: {selectOption || ''}</p> 
+
+      
+        
     </div>
   )
 }
---
+++ src/components/ChildComponent1.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ChildComponent1 = ({ onSelect }) => {
+  return (
+    <div className="child child1">
+      <h2>Child Component 1</h2>
+      <button onClick={() => onSelect('Option 1')}>Option 1</button>
+    </div>
+  );
+};
+
+
+export default ChildComponent1;
